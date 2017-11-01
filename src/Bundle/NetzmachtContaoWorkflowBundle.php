f15623ca7af940fc33e4069e5dbafc8642e0f682
David Molineus <david.molineus@netzmacht.de>
2017-11-01 16:52:06 +0100

    Move resources to bundle namespace.

---
+++ src/Bundle/NetzmachtContaoWorkflowBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * contao-workflow.
+ *
+ * @package    contao-workflow
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @license    LGPL-3.0 https://github.com/netzmacht/contao-leaflet-maps/blob/master/LICENSE
+ * @filesource
+ */
+
+namespace Netzmacht\Contao\Workflow\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class NetzmachtContaoWorkflowBundle
+ *
+ * @package Netzmacht\Contao\Workflow
+ */
+class NetzmachtContaoWorkflowBundle extends Bundle
+{
+
+}
