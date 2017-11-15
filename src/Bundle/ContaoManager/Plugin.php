4467c532e6ffc3d84554f2cd8b776553ca66074f
David Molineus <david.molineus@netzmacht.de>
2017-11-15 14:56:37 +0100

    Add contao manager plugin

---
+++ src/Bundle/ContaoManager/Plugin.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This Contao-Workflow extension allows the definition of workflow process for entities from different providers. This
+ * extension is a workflow framework which can be used from other extensions to provide their custom workflow handling.
+ *
+ * @package    workflow
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ */
+
+namespace Netzmacht\Contao\Workflow\Bundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Netzmacht\Contao\Toolkit\Bundle\NetzmachtContaoToolkitBundle;
+use Netzmacht\Contao\Workflow\Bundle\NetzmachtContaoWorkflowBundle;
+
+/**
+ * Class Plugin
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(NetzmachtContaoWorkflowBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class, NetzmachtContaoToolkitBundle::class])
+                ->setReplace(['workflow'])
+        ];
+    }
+}
