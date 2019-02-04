3ceaf08a41dd5e51d8c7ab7116af5e742e837173
David Molineus <david.molineus@netzmacht.de>
2019-02-04 14:30:41 +0100

    Implement a notification action using the notification center.

---
+++ src/Workflow/Flow/Action/Notification/NotificationAction.php
@@ -0,0 +1,192 @@
+<?php
+
+/**
+ * This Contao-Workflow extension allows the definition of workflow process for entities from different providers. This
+ * extension is a workflow framework which can be used from other extensions to provide their custom workflow handling.
+ *
+ * @package    workflow
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014-2019 netzmacht David Molineus
+ * @license    LGPL 3.0-or-later
+ * @filesource
+ */
+
+declare(strict_types=1);
+
+namespace Netzmacht\ContaoWorkflowBundle\Workflow\Flow\Action\Notification;
+
+use Contao\StringUtil;
+use Netzmacht\Contao\Toolkit\Data\Model\RepositoryManager;
+use Netzmacht\ContaoWorkflowBundle\PropertyAccess\PropertyAccessManager;
+use Netzmacht\ContaoWorkflowBundle\Workflow\Flow\Action\AbstractAction;
+use Netzmacht\Workflow\Flow\Context;
+use Netzmacht\Workflow\Flow\Item;
+use Netzmacht\Workflow\Flow\State;
+use Netzmacht\Workflow\Flow\Transition;
+use NotificationCenter\Model\Notification;
+use Symfony\Component\Translation\TranslatorInterface as Translator;
+
+/**
+ * Class NotificationAction send an notification as post action.
+ */
+final class NotificationAction extends AbstractAction
+{
+    public const STATE_ALL = 3;
+    public const STATE_SUCCESS = 1;
+    public const STATE_FAILED = 2;
+
+    /**
+     * Property access manager.
+     *
+     * @var PropertyAccessManager
+     */
+    private $propertyAccess;
+
+    /**
+     * Repository manager.
+     *
+     * @var RepositoryManager
+     */
+    private $repositoryManager;
+
+    /**
+     * Translator.
+     *
+     * @var Translator
+     */
+    private $translator;
+
+    /**
+     * Success states.
+     *
+     * @var int
+     */
+    private $successStates;
+
+    /**
+     * Notification ids.
+     *
+     * @var int
+     */
+    private $notificationId;
+
+    /**
+     * Construct.
+     *
+     * @param PropertyAccessManager $propertyAccess    Property access manager.
+     * @param RepositoryManager     $repositoryManager Repository manager-
+     * @param Translator            $translator        Translator.
+     * @param string                $name              Name of the element.
+     * @param string                $label             Label of the element.
+     * @param int                   $notificationId    Notification id.
+     * @param int                   $successStates     Success states.
+     * @param array                 $config            Configuration values.
+     */
+    public function __construct(
+        PropertyAccessManager $propertyAccess,
+        RepositoryManager $repositoryManager,
+        Translator $translator,
+        string $name,
+        string $label,
+        int $notificationId,
+        int $successStates = self::STATE_ALL,
+        array $config = []
+    ) {
+        parent::__construct($name, $label, $config);
+
+        $this->repositoryManager = $repositoryManager;
+        $this->propertyAccess    = $propertyAccess;
+        $this->translator        = $translator;
+        $this->notificationId    = $notificationId;
+        $this->successStates     = $successStates;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRequiredPayloadProperties(Item $item): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validate(Item $item, Context $context): bool
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function transit(Transition $transition, Item $item, Context $context): void
+    {
+        $latestState = $item->getLatestState(false);
+        if ($latestState === false || !$this->matchSuccessState($latestState)) {
+            return;
+        }
+
+        $notification = $this->repositoryManager->getRepository(Notification::class)->find($this->notificationId);
+        if (!$notification instanceof Notification) {
+            return;
+        }
+
+        $notification->send($this->buildTokens($transition, $item, $context));
+    }
+
+    private function matchSuccessState(State $latestState): bool
+    {
+        if ($latestState->isSuccessful()) {
+            return ($this->successStates & self::STATE_SUCCESS) === self::STATE_SUCCESS;
+        }
+
+        return ($this->successStates & self::STATE_FAILED) === self::STATE_FAILED;
+    }
+
+    private function buildTokens(Transition $transition, Item $item, Context $context): array
+    {
+        $workflow        = $transition->getWorkflow();
+        $currentStepName = $item->getCurrentStepName();
+        $latestState     = $item->getLatestState(false);
+        $step            = null;
+        $entity          = $item->getEntity();
+        $tokens          = [
+            'transition_name' => $transition->getName(),
+            'successful'      => $latestState
+                ? $this->translator->trans('workflow.successful.yes')
+                : $this->translator->trans('workflow.successful.no'),
+        ];
+
+        foreach ($transition->getConfig() as $name => $value) {
+            $tokens['transition_' . $name] = StringUtil::deserialize($value);
+        }
+
+        if ($workflow->hasStep($currentStepName)) {
+            $step                = $workflow->getStep($currentStepName);
+            $tokens['step_name'] = $step->getName();
+
+            foreach ($step->getConfig() as $name => $value) {
+                $tokens['step_' . $name] = StringUtil::deserialize($value);
+            }
+        }
+
+        foreach ($context->getProperties() as $name => $value) {
+            $tokens['property_' . $name] = $value;
+        }
+
+        foreach ($context->getPayload() as $name => $value) {
+            $tokens['payload_' . $name] = $value;
+        }
+
+        if ($this->propertyAccess->supports($entity)) {
+            $propertyAccessor = $this->propertyAccess->provideAccess($entity);
+
+            foreach ($propertyAccessor as $name => $value) {
+                $tokens['entity_' . $name] = StringUtil::deserialize($value);
+            }
+        }
+
+        return $tokens;
+    }
+}
---
+++ src/Workflow/Flow/Action/Notification/NotificationActionFactory.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * This Contao-Workflow extension allows the definition of workflow process for entities from different providers. This
+ * extension is a workflow framework which can be used from other extensions to provide their custom workflow handling.
+ *
+ * @package    workflow
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014-2019 netzmacht David Molineus
+ * @license    LGPL 3.0-or-later
+ * @filesource
+ */
+
+declare(strict_types=1);
+
+namespace Netzmacht\ContaoWorkflowBundle\Workflow\Flow\Action\Notification;
+
+use Contao\StringUtil;
+use Netzmacht\Contao\Toolkit\Data\Model\RepositoryManager;
+use Netzmacht\ContaoWorkflowBundle\PropertyAccess\PropertyAccessManager;
+use Netzmacht\ContaoWorkflowBundle\Workflow\Flow\Action\ActionTypeFactory;
+use Netzmacht\Workflow\Flow\Action;
+use Netzmacht\Workflow\Flow\Transition;
+use Netzmacht\Workflow\Flow\Workflow;
+use function in_array;
+use Symfony\Component\Translation\TranslatorInterface as Translator;
+use Symfony\Component\Translation\TranslatorInterface;
+
+/**
+ * Class NotificationActionFactory
+ */
+final class NotificationActionFactory implements ActionTypeFactory
+{
+    /**
+     * Repository manager.
+     *
+     * @var RepositoryManager
+     */
+    private $repositoryManager;
+
+    /**
+     * Property access manager.
+     *
+     * @var PropertyAccessManager
+     */
+    private $propertyAccessManager;
+
+    /**
+     * Translator.
+     *
+     * @var Translator
+     */
+    private $translator;
+
+    /**
+     * NotificationActionFactory constructor.
+     *
+     * @param RepositoryManager     $repositoryManager     Repository manager.
+     * @param PropertyAccessManager $propertyAccessManager Property access manager.
+     * @param TranslatorInterface   $translator            Translator.
+     */
+    public function __construct(
+        RepositoryManager $repositoryManager,
+        PropertyAccessManager $propertyAccessManager,
+        Translator $translator
+    ) {
+        $this->repositoryManager     = $repositoryManager;
+        $this->propertyAccessManager = $propertyAccessManager;
+        $this->translator            = $translator;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCategory(): string
+    {
+        return 'default';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName(): string
+    {
+        return 'notification';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isPostAction(): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports(Workflow $workflow): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function create(array $config, Transition $transition): Action
+    {
+        return new NotificationAction(
+            $this->propertyAccessManager,
+            $this->repositoryManager,
+            $this->translator,
+            'action_' . $config['id'],
+            $config['label'],
+            (int) $config['notification_id'],
+            $this->determineSuccessStates($config['notification_states'])
+        );
+    }
+
+    /**
+     * Determine the success states.
+     *
+     * @param mixed $states Raw success state value.
+     *
+     * @return int
+     */
+    private function determineSuccessStates($states): int
+    {
+        $value  = 0;
+        $states = StringUtil::deserialize($states, true);
+
+        if (in_array('success', $states, true)) {
+            $value |= NotificationAction::STATE_SUCCESS;
+        }
+
+        if (in_array('failed', $states, true)) {
+            $value |= NotificationAction::STATE_FAILED;
+        }
+
+        return $value;
+    }
+}
