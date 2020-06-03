44bfae3c172e1380fa33d563b8a67b60af7e14cc
David Molineus <david.molineus@netzmacht.de>
2020-06-03 09:28:20 +0200

    Update code style

--- spec/PropertyAccess/ContaoModelPropertyAccessorSpec.php
+++ spec/PropertyAccess/ContaoModelPropertyAccessorSpec.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This Contao-Workflow extension allows the definition of workflow process for entities from different providers. This
+ * extension is a workflow framework which can be used from other extensions to provide their custom workflow handling.
+ *
+ * @package    workflow
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014-2020 netzmacht David Molineus
+ * @license    LGPL 3.0-or-later
+ * @filesource
+ */
+
+declare(strict_types=1);
+
 namespace spec\Netzmacht\ContaoWorkflowBundle\PropertyAccess;
 
 use Contao\Model;
