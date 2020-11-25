9054238184b96903dc2f87711e2e7e2ab710b477
David Molineus <david.molineus@netzmacht.de>
2020-11-25 11:45:52 +0100

    Render data record label

--- src/Workflow/View/Renderer/TeaserRenderer.php
+++ src/Workflow/View/Renderer/TeaserRenderer.php
@@ -15,9 +15,12 @@ declare(strict_types=1);
 
 namespace Netzmacht\ContaoWorkflowBundle\Workflow\View\Renderer;
 
+use Netzmacht\ContaoWorkflowBundle\PropertyAccess\PropertyAccessManager;
 use Netzmacht\ContaoWorkflowBundle\Workflow\View\View;
+use Netzmacht\Workflow\Flow\Item;
 use Netzmacht\Workflow\Flow\Step;
 use Netzmacht\Workflow\Flow\Transition;
+use Symfony\Component\Translation\TranslatorInterface as Translator;
 
 /**
  * Class TransitionTeaserRenderer
@@ -32,11 +35,39 @@ final class TeaserRenderer extends AbstractRenderer
     protected static $section = 'teaser';
 
     /**
+     * Property access manager.
+     *
+     * @var PropertyAccessManager
+     */
+    private $propertyAccessManager;
+
+    /**
+     * AbstractRenderer constructor.
+     *
+     * @param Translator            $translator            Translator.
+     * @param array                 $templates             Mapping between the content type and the default template.
+     * @param PropertyAccessManager $propertyAccessManager Property access manager.
+     *
+     * @throws \Assert\AssertionFailedException If No section name is defined.
+     */
+    public function __construct(
+        Translator $translator,
+        PropertyAccessManager $propertyAccessManager,
+        array $templates = []
+    ) {
+        parent::__construct($translator, $templates);
+
+        $this->propertyAccessManager = $propertyAccessManager;
+    }
+
+    /**
      * {@inheritDoc}
      */
     public function supports(View $view): bool
     {
-        return $view->getContext() instanceof Transition || $view->getContext() instanceof Step;
+        return $view->getContext() instanceof Transition
+            || $view->getContext() instanceof Step
+            || $view->getContext() === null;
     }
 
     /**
@@ -48,18 +79,64 @@ final class TeaserRenderer extends AbstractRenderer
         $context     = $view->getContext();
         $workflow    = $view->getWorkflow();
         $stepName    = $view->getItem()->getCurrentStepName();
+        $item        = $view->getItem();
         $currentStep = null;
 
-        if ($workflow->hasStep($stepName)) {
+        if ($stepName && $workflow->hasStep($stepName)) {
             $currentStep = $workflow->getStep($stepName);
         }
 
         return [
-            'headline'    => $context->getLabel(),
-            'description' => $context->getConfigValue('description'),
+            'headline'    => $context ? $context->getLabel() : $workflow->getLabel(),
+            'description' => $context ? $context->getConfigValue('description') : null,
             'workflow'    => $view->getWorkflow(),
             'currentStep' => $currentStep,
-            'item'        => $view->getItem()
+            'item'        => $view->getItem(),
+            'entityLabel' => $this->renderEntityLabel($item),
         ];
+    }
+
+    /**
+     * Render the data record label.
+     *
+     * @param Item $item The current item.
+     *
+     * @return string|null
+     */
+    private function renderEntityLabel(Item $item): ?string
+    {
+        $entity   = $item->getEntity();
+        $entityId = $item->getEntityId();
+
+        if (! $this->propertyAccessManager->supports($entity)) {
+            return 'ID ' . $entityId->getIdentifier();
+        }
+
+        $accessor = $this->propertyAccessManager->provideAccess($entity);
+        $label    = null;
+        foreach (['title', 'name', 'headline'] as $labelField) {
+            if (!$accessor->has($labelField)) {
+                continue;
+            }
+
+            $label = $accessor->get($labelField);
+            if ($label !== null) {
+                break;
+            }
+        }
+
+        if (!$label) {
+            $label = 'ID ' . $entityId->getIdentifier();
+        } else {
+            $label = sprintf('%s (ID %s)', $label, $entityId->getIdentifier());
+        }
+
+        $providerKey   = 'MOD.' . $item->getEntityId()->getProviderName();
+        $providerLabel = $this->trans($providerKey, [], 'contao_modules');
+        if ($providerLabel === $providerKey) {
+            return $label;
+        }
+
+        return sprintf('%s: %s', $providerLabel, $label);
     }
 }
